e37b987d650a656c67cb8ff1d3eab9c93eb723bf
davidbabescu <david.babesccu@gmail.com>
2023-04-01 22:21:13 +0300

    Functional Chatbot v1

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Configuration, OpenAIApi } = require ('openai');
+const express = require ('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const token = process.env.API_TOKEN;
+const configuration = new Configuration ( {apiKey: token} );
+const openai = new OpenAIApi( configuration );
+const path = require('path');
+const { log } = require('console');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+app.use(express.static('public'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+app.post('/message', (req, res) => {
+    console.log('post message', req.body.prompt);
+    const response = openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: 'write me a poem about' + req.body.prompt,
+        temperature: 0,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        max_tokens: 1024
+    })
+
+    response.then((data) => {
+        const message = {message: data.data.choices[0].text};
+        res.send(message);
+
+    }).catch((err) => {
+        console.log('err', err);
+        res.send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+  
---
+++ public/main.js
@@ -0,0 +1,55 @@
+const chatBox = document.querySelector(".chat-box");
+const inputField = chatBox.querySelector("input[type='text']");
+const button = chatBox.querySelector("button");
+console.log('ok', button);
+const chatBoxBody = chatBox.querySelector(".chat-box-body");
+console.log('body', chatBoxBody);
+
+button.addEventListener("click", sendMessage);
+inputField.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
+
+function sendMessage() {
+  console.log('send message');
+   const message = inputField.value;
+  // inputField.value = "";
+  // chatBoxBody.innerHTML += `<div class="message"><p>${message}</p></div>`;
+  // chatBoxBody.innerHTML += `<div id="loading" class="response loading">.</div>`;
+  // scrollToBottom();
+  // window.dotsGoingUp = true;
+  //   var dots = window.setInterval( function() {
+  //       var wait = document.getElementById("loading");
+  //       if ( window.dotsGoingUp ) 
+  //           wait.innerHTML += ".";
+  //       else {
+  //           wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+  //       if ( wait.innerHTML.length < 2)
+  //           window.dotsGoingUp = true;
+  //       }
+  //       if ( wait.innerHTML.length > 3 )
+  //           window.dotsGoingUp = false;
+  //       }, 250);
+
+  fetch('http://localhost:3000/message', {
+    method: 'POST',
+    headers: {
+      accept: 'application.json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({prompt: message})
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    console.log('data', data);
+    //document.getElementById("loading").remove();
+    chatBoxBody.innerHTML += `<div class="response"><p>${data.message}</p></div>`;
+    scrollToBottom();
+  })
+}
+
+function scrollToBottom() {
+  chatBoxBody.scrollTop = chatBoxBody.scrollHeight;
+}
